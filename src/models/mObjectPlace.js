9bbc8ee4ead2fff040ef8c9b9157cf16e2e1385c
danielsicruz <dscrz@hotmail.com>
2023-08-01 22:32:52 -0300

    Create class ObjectPlace

---
+++ src/models/mObjectPlace.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize')
+const database = require('./db')
+
+const place = require("./mPlace")
+const object = require("./mObject")
+const user = require("./mUser")
+
+const Object = database.define('ObjectPlace', {
+    idObject: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: object,
+            key: "id"
+        }
+    },
+    idPlace: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: place,
+            key: "id"
+        }
+    },
+    stillThere: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: 1,
+    },
+    movedBy: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: user,
+            key: "id"
+        }
+    }
+
+})

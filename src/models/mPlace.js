7dcd5f4fbf903de5cdbf4754a6c15dd39d3146b8
danielsicruz <dscrz@hotmail.com>
2023-08-01 22:33:19 -0300

    Create class Place

---
+++ src/models/mPlace.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+const database = require('./db')
+
+const context = require('./mContext')
+const Object = database.define('Place',{
+    id:{
+        type:Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    vid:{
+        type:Sequelize.STRING,
+    },
+    name:{
+        type: Sequelize.STRING
+    },
+    context:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+            model:context,
+            key:'id'
+        }
+    }
+    
+})

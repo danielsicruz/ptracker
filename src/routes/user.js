e725b1a4f54e21829c54a41e4eee80ef966e09f7
danielsicruz <dscrz@hotmail.com>
2023-08-02 08:08:28 -0300

    Create user route

---
+++ src/routes/user.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { route } = require("express/lib/application");
+let router = express.Router();
+const userService = require("../services/userService");
+
+router.post("/", userService.create);
+router.get("/", userService.select);
+router.put("/:id", userService.update);
+//srouter.delete("/:id", userService.delete)
+
+module.exports = router;

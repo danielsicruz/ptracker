532c25df900a5261cd8886fb4eb28e350b759766
danielsicruz <dscrz@hotmail.com>
2023-08-03 21:18:52 -0300

    Create object service

---
+++ src/services/objectService.js
@@ -0,0 +1,56 @@
+const objectController = require("../controllers/objectController");
+
+exports.create = async (req, res) => {
+    data = req.body;
+    console.log(data);
+    //data.user_image = "/images/"+req.file.originalname;
+    //rules
+    if (true) {
+        object = await objectController.create(data, res);
+        return res.status(201).json(object);
+    } else {
+        return res.status(401).json({ 'message': 'Unauthorized' });
+    }
+}
+
+exports.update = async (req,res) => {
+    data = req.body;
+    filter = {
+        name: data.name ? data.name : null,
+        description: data.description ? data.description : null,
+        imagePath: data.imagePath ? data.imagePath : null,
+        dispached: data.dispached ? data.dispached : null, //Test because is boolean
+        created_at: data.created_at ? data.created_at : null,
+        updated_at: data.updated_at ? data.updated_at : null,
+    }
+    Object.keys(filter).forEach(key => {
+        if (filter[key] == null) {
+            delete filter[key];
+        }
+        
+    });
+
+    if (req.query.filter == undefined) {
+
+        //We should create the 'filter' param to check if have filters and later get
+        //all the params to filter the response
+        console.log("No filter");
+        //rules
+        if (true) {
+            users = await userController.select(null, res);
+            return res.status(200).json(users);
+        } else {
+            return res.status(401).json({ 'message': 'Unauthorized' });
+        }
+    } else {
+        //rules
+        if (true) {
+            //return res.status(200).json(filter);
+
+            users = await userController.select(filter, res);
+            return res.status(200).json(users);
+        } else {
+            return res.status(401).json({ 'message': 'Unauthorized' });
+        }
+    }
+}

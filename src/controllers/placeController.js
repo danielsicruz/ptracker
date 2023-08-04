5f65df30aeb56d8397ffd511ff6b2311d62f4684
danielsicruz <dscrz@hotmail.com>
2023-08-04 00:50:49 -0300

    create place controller

---
+++ src/controllers/placeController.js
@@ -0,0 +1,52 @@
+const Place = require("../models/mPlace")
+
+exports.create = async (data) => {
+    response = await Place.create({
+        name: data.name,
+        vid: data.vid,
+        context: data.context
+    });
+
+    return response;
+}
+
+exports.select = async (filters) => {
+    let response;
+    if (filters == null) {
+        response = await Place.findAll();
+    } else {
+        response = await Place.findAll({ where: filters })
+    }
+    return response;
+}
+
+exports.selectOne = async (filters) => {
+    response = await Place.findOne({
+        where: filters
+    });
+    return response;
+}
+
+exports.findById = async (id) => {
+    response = await Place.findOne({
+        where: { id: id }
+    });
+}
+
+exports.update = async (data) => {
+    const tochange = Place.findByPk(data.id);
+    tochange = data.name ? data.name : tochange.name;
+    tochange = data.vid ? data.vid : tochange.vid;
+    tochange = data.context ? data.context : tochange.context;
+
+    response = await tochange.save();
+    return response;
+}
+
+exports.delete = async (data) => {
+    return Place.destroy({
+        where: {
+            id: data.id
+        }
+    });
+}

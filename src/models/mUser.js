a0339c5dd8b624717c72ff1de81c05263b5d7ff1
danielsicruz <dscrz@hotmail.com>
2023-08-04 00:53:34 -0300

    add softdelete to model user

--- src/models/mUser.js
+++ src/models/mUser.js
@@ -1,26 +1,26 @@
 const Sequelize = require('sequelize')
 const database = require('./db')
 
-const User = database.define('User',{
-    id:{
-        type:Sequelize.INTEGER,
+const User = database.define('User', {
+    id: {
+        type: Sequelize.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncremet: true
     },
-    name:{
+    name: {
         type: Sequelize.STRING
     },
-    login:{
-        type:Sequelize.STRING
+    login: {
+        type: Sequelize.STRING
     },
-    password:{
-        type:Sequelize.TEXT
+    password: {
+        type: Sequelize.TEXT
     },
-    active:{
-        type:Sequelize.BOOLEAN
+    active: {
+        type: Sequelize.BOOLEAN
     }
-    
-})
+
+}, { paranoid: true });
 
 module.exports = User

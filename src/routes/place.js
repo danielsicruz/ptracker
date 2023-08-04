16668bc824885c33af6e877f23d9f296c18bc3dd
danielsicruz <dscrz@hotmail.com>
2023-08-04 00:55:14 -0300

    create place route

---
+++ src/routes/place.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { route } = require("express/lib/application");
+let router = express.Router();
+const placeService = require("../services/placeService");
+
+router.post("/", placeService.create);
+router.get("/", placeService.select);
+router.put("/:id", placeService.update);
+//srouter.delete("/:id", placeService.delete)
+
+module.exports = router;

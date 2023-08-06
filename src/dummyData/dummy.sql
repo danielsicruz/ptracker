d4b0f5f7894c46d0006ca3b847bb5b8cb22d076d
danielsicruz <dscrz@hotmail.com>
2023-08-06 00:24:01 -0300

    Test data

---
+++ src/dummyData/dummy.sql
@@ -0,0 +1,4 @@
+INSERT INTO Contexts(`id`, `name`, `createdAt`, `updatedAt`) VALUES (1, 'Escola','2023-05-08 23:45','2023-05-08 23:45' );
+
+
+INSERT INTO Users(`id`, `name`, `login`, `password`, `active`, `createdAt`,`updatedAt`) VALUES(1,'Daniel', 'ss1085219','1234',1,'2023-05-08 23:45','2023-05-08 23:45');

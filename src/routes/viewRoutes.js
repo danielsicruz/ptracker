2e04e5d6fad8739527fa0288e03a8e2ad1b2db8c
danielsicruz <dscrz@hotmail.com>
2023-08-06 00:27:38 -0300

    create viewRoutes file

---
+++ src/routes/viewRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/login", (req, res) =>{
+    res.render('login');
+});
+router.get("/home", (req, res) =>{
+    res.render('home');
+});
+router.get("/place", (req, res) =>{
+    res.render('place');
+});
+
+module.exports = router;

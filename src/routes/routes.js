dcfa1f0c83177ae5506fe1e9008c613c7fdb9c33
danielsicruz <dscrz@hotmail.com>
2023-08-06 23:31:36 -0300

    Add movement an check routes to be required in app.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,11 +6,15 @@ const object = require("./object")
 const objectPlace = require("./objectPlace")
 const place = require("./place")
 const context = require("./context")
+const movement = require("./movement")
+const check = require("./check")
 
 router.use('/v1/user', user);
 router.use('/v1/object', object);
 router.use('/v1/objectplace', objectPlace);
 router.use('/v1/place', place);
 router.use('/v1/context', context);
+router.use('/v1/movement', movement);
+router.use('/v1/check', check);
 
 module.exports = router;

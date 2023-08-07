83937fccc7976040ce32b55332a33a7b932f894f
danielsicruz <dscrz@hotmail.com>
2023-08-06 23:26:03 -0300

    Create Movement controller

---
+++ src/controllers/movementController.js
@@ -0,0 +1,52 @@
+const Movement = require("../models/mMovement")
+
+exports.create = async (data) => {
+    response = await Movement.create({
+        idObject: data.idObject,
+        fromIdPlace: data.fromIdPlace,
+        toIdPlace: data.toIdPlace,
+        whoChanged: data.whoChanged,
+        temporary: data.temporary,
+        whenBack: data.whenBack,
+        isBack: data.isBack,
+    });
+
+    return response;
+}
+
+exports.select = async (filters) => {
+    let response;
+    if (filters == null) {
+        response = await Movement.findAll();
+    } else {
+        response = await Movement.findAll({ where: filters })
+    }
+    return response;
+}
+
+exports.selectOne = async (filters) => {
+    response = await Movement.findOne({
+        where: filters
+    });
+    return response;
+}
+
+exports.findById = async (id) => {
+    response = await Movement.findOne({
+        where: { id: id }
+    });
+}
+
+exports.update = async (data) => {
+    const tochange = Movement.findByPk(data.id);
+    tochange = data.idObject ? data.idObject : tochange.idObject;
+    tochange = data.fromIdPlace ? data.fromIdPlace : tochange.fromIdPlace;
+    tochange = data.toIdPlace ? data.toIdPlace : tochange.toIdPlace;
+    tochange = data.whoChanged ? data.whoChanged : tochange.whoChanged;
+    tochange = data.temporary ? data.temporary : tochange.temporary; //Test because is boolean
+    tochange = data.whenBack ? data.whenBack : tochange.whenBack;
+    tochange = data.isBack ? data.isBack : tochange.isBack; //Test because is boolean
+
+    response = await tochange.save();
+    return response;
+}

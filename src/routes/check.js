0a39710f7a13f361cc777e42f3faa51de4056757
danielsicruz <dscrz@hotmail.com>
2023-08-06 23:29:49 -0300

    Create Check routes

---
+++ src/routes/check.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { route } = require("express/lib/application");
+let router = express.Router();
+const checkService = require("../services/checkService");
+
+router.post("/", checkService.create);
+router.get("/", checkService.select);
+router.get("/test", checkService.test);
+router.put("/:id", checkService.update);
+//srouter.delete("/:id", checkService.delete)
+
+module.exports = router;

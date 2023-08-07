9cdadf3465ba82178b78ac9fcdf15f930d71be55
danielsicruz <dscrz@hotmail.com>
2023-08-06 23:33:48 -0300

    increasing view routes and make data come from DTO

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/login", (req, res) =>{
+const data = require('../dtos')
+
+router.get("/login", (req, res) => {
     res.render('login');
 });
-router.get("/home", (req, res) =>{
+router.get("/home", async (req, res) => {
+    const homeData = await data.place.data();
     res.render('home');
 });
-router.get("/place", (req, res) =>{
-    res.render('place',{places:[]});
+router.get("/place", async (req, res) => {
+    const placeData = await data.place.data();
+    res.render('place', {
+        "datas": placeData
+    });
 });
 
 module.exports = router;

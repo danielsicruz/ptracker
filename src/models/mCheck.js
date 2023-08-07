4c642a886dc566fd740d6dc8895fbdf7928b0cbc
danielsicruz <dscrz@hotmail.com>
2023-08-06 23:28:47 -0300

    Create Check model

---
+++ src/models/mCheck.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize')
+const database = require('./db')
+const Check = database.define('Check', {
+    id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    objectsToFind: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    foundObjects: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    missingObjects: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    }
+});
+
+module.exports = Check

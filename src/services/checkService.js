e3592e11f05ca695a7b81ac3e7b9cc2f1b139aa9
danielsicruz <dscrz@hotmail.com>
2023-08-15 18:58:29 -0300

    Add create check and make all logic on check places

--- src/services/checkService.js
+++ src/services/checkService.js
@@ -1,10 +1,118 @@
 const checkController = require("../controllers/checkController");
+const objectPlaceController = require("../controllers/objectPlaceController");
+const movementController = require("../controllers/movementController");
 const Place = require("../models/mPlace");
 const User = require("../models/mUser");
+const { Op } = require("sequelize");
 exports.create = async (req, res) => {
     data = req.body;
-    const placeId = req.params.id;
-    console.log(data);
+    let objectsMatches = 0;
+    const idPlace = req.params.id;
+    //Select objects to find. Deeeerrr
+    const objectsToFind = await objectPlaceController.select({ idPlace: idPlace });
+
+    const wrongPlaceObjectsid = Array();
+
+    //If option selected is move not foundobjects to another room
+    if (data.notFoundObjects === 1) {
+        const wrongPlaceObjectsToMovements = Array();
+        //Create a query to find no back movements of this room objects
+        const queryBuilder = {
+            idObject: { [Op.or]: data.objects },
+            temporary: 1,
+            isBack: 0
+        }
+        //Select them. Deeerrrr
+        const movements = await movementController.select(queryBuilder);
+        //I don't need to say what a forEach does.
+        objectsToFind.forEach(object => {
+            //If the object is in the array of objects to find
+            if (data.objects.includes(object.idObject.toString())) {
+                //Count the number of matches and set that object is there
+                objectsMatches++;
+                object.isThere = 1;
+                //If the object is in the array of movements
+            } else if (movements.length > 0) {
+                //For each movement and push to wrongPlaceObjects
+                movements.forEach(movement => {
+                    if (movement.idObject === object.idObject)
+                        wrongPlaceObjectsToMovements.push({ idObject: object.idObject, whereIs: movement.toIdPlace });
+                    wrongPlaceObjectsid.push({ idObject: object.idObject, whereIs: movement.toIdPlace });
+                });
+            } else {
+                //Set isThere to 0. Deeeerrrr
+                //And push to wrongPlaceObjects
+                object.isThere = 0;
+                wrongPlaceObjectsid.push({ idObject: object.idObject });
+            }
+        });
+        //Move not found objects to another room if have a no back object movement
+        const movementQueryBuilder = Array();
+        wrongPlaceObjectsToMovements.forEach(object => {
+            movementQueryBuilder.push({
+                idObject: object.idObject,
+                idPlace: object.whereIs,
+                isBack: null,
+                temporary: null,
+                isThere: 1,
+
+            });
+        });
+        await movementController.bulkCreate(movementQueryBuilder)
+    } else {
+        //Just set the isThere to 0 for all not found objects and push
+        objectsToFind.forEach(object => {
+            object.isThere = 0;
+            if (data.objects.includes(object.idObject.toString())) {
+                objectsMatches++;
+                object.isThere = 1;
+            } else {
+                wrongPlaceObjectsid.push({idObject : object.idObject});
+            }
+
+        });
+    }
+    //If option selected is keep wrong place objects on their place (not this place dude, their original place)
+    if (data.wrongPlaceObjects === 0) {
+        //Set isThere to 0 for all wrong place objects and update on database
+        const whereQueryBuilder = {
+            idObject: { [Op.or]: wrongPlaceObjectsid },
+        };
+        await objectPlaceController.updateMultiple({ whereIs: idPlace, isThere: 0 }, whereQueryBuilder);
+
+    }
+    else {
+        //Bring objects to this place
+        const queryBuilder = Array();
+        wrongPlaceObjectsid.forEach(object => {
+            queryBuilder.push({
+                idObject: object,
+                idPlace: idPlace,
+                isBack: null,
+                temporary: null,
+                isThere: 1,
+            });
+        }
+        );
+        movementController.bulkCreate(queryBuilder);
+    }
+    const objectPlaceBuilder = {
+        idObject: null,
+        idPlace: idPlace,
+        stillThere: null,
+        movedBy: null,
+        lastCheck: null,
+    }
+    const checkBuilder = {
+        objectsToFind: objectsToFind.length,
+        foundObjects: data.objects.length,
+        missingObjects: (objectsToFind.length - objectsMatches),
+        whoChecked: null,
+        whereChecked: parseInt(idPlace),
+        notFoundObjects: data.notFoundObjects,
+        wrongPlaceObjects: data.wrongPlaceObjects
+    }
+    console.log();
     return res.status(201).json();
     if (true) {
         check = await checkController.create(data);
@@ -13,7 +121,7 @@ exports.create = async (req, res) => {
         return res.status(401).json({ 'message': 'Unauthorized' });
     }
 }
-exports.select = async (req,res) => {
+exports.select = async (req, res) => {
     data = req.body;
     filter = {
         id: data.id ? data.id : null,
@@ -27,7 +135,7 @@ exports.select = async (req,res) => {
         if (filter[key] == null) {
             delete filter[key];
         }
-        
+
     });
 
     if (req.query.filter == undefined) {
@@ -64,7 +172,7 @@ exports.update = async (req, res) => {
 
     } else {
         return res.status(401).json({ 'message': 'Unauthorized' });
-    }    
+    }
 }
 
 exports.test = async (req, res) => {
@@ -72,11 +180,11 @@ exports.test = async (req, res) => {
         include: [
             {
                 model: User,
-                
+
             },
             {
                 model: Place,
-                
+
             },
         ],
     }

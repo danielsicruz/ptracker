c9d5caf7ccdd5dba3598d53d803c304171e8ad1b
danielsicruz <dscrz@hotmail.com>
2023-08-15 18:55:20 -0300

    Comment relationships and add whereIs attribute on objectPlace

--- src/models/initialize.js
+++ src/models/initialize.js
@@ -8,19 +8,25 @@ const Context = require('./mContext');
 const ObjectPlace = require('./mObjectPlace');
 const Place = require('./mPlace');
 
+//Relations in Check table with User and Place
 Check.belongsTo(User,{foreignKey: 'whoChecked'});
 User.hasMany(Check, {as:'whoChecked',foreignKey: 'whoChecked'});
 Check.belongsTo(Place,{foreignKey: 'whereChecked',});
 Place.hasMany(Check, {as:'whereChecked',foreignKey: 'whereChecked'});
 
+//Relations in Context table with Place
 Context.hasMany(Place, {as:'context',foreignKey: 'context'});
 Place.belongsTo(Context,{foreignKey: 'context'});
 
+//Relations in ObjectPlace table with Object and Place
 ObjectPlace.belongsTo(Object, {foreignKey:'idObject'});
-Object.hasMany(ObjectPlace,{as:'objectInPlace',foreignKey:'idObject'})
+Object.hasMany(ObjectPlace,{as:'objectInPlace',foreignKey:'idObject'});
 ObjectPlace.belongsTo(Place,{foreignKey:'idPlace'});
-Place.hasMany(ObjectPlace,{as:'place',foreignKey:'idObject'})
+Place.hasMany(ObjectPlace,{as:'place',foreignKey:'idObject'});
+ObjectPlace.belongsTo(Place,{foreignKey:'whereIs', allowNull: true});
+Place.hasMany(ObjectPlace,{as:'whereIs',foreignKey:'whereIs', allowNull: true});
 
+//Relations in Movement table with Object, Place, User
 Movement.belongsTo(Object,{foreignKey:'idObject'});
 Object.hasMany(Movement, {as:'movement',foreignKey:'idObject'});
 Movement.belongsTo(Place, {foreignKey:'fromIdPlace'});

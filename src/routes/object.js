106995db37737cba4158dcf79d28d7a5a1bc9737
danielsicruz <dscrz@hotmail.com>
2023-08-28 02:12:36 -0300

    Setting Multer for image upload

--- src/routes/object.js
+++ src/routes/object.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const { route } = require("express/lib/application");
 let router = express.Router();
+const multer = require("multer");
+const path = require("path");
 const objectService = require("../services/objectService");
 
-router.post("/", objectService.create);
+const storage = multer.diskStorage({
+    destination: './public/images/objects/',
+    filename: function (req, file, cb) {
+        const id = req.body.id;
+        cb(null, id + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/", upload.single('image'), objectService.create);
 router.get("/", objectService.select);
 router.get("/:id", objectService.getOne);
 router.put("/:id", objectService.update);
